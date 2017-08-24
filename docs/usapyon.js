55a659444edeb00421126626f8addcbcc0d95bd3
cicada_sss <sss.cicada@gmail.com>
2017-08-24 13:49:42 +0900

    実績データ読み込み関係分割

--- docs/usapyon.js
+++ docs/usapyon.js
@@ -1,3 +1,32 @@
+//初期化
+var usagi = 0;
+var kuma = 0;
+var risu = 0;
+var aja = 0;
+var tairyou = 0;
+var n = 1;
+var n2 = 1;
+var sW2 = window.innerWidth + 60;
+var sH2 = window.innerHeight + 100;
+var creimg;
+var usaran = ["image/usa (1).png", "image/usa (2).png", "image/usa (3).png", "image/usa (4).png", "image/usa (5).png", "image/usa (6).png", "image/usa (7).png"];
+for (var i = 0; i < 20; i++) {
+    var key = localStorage.key(i); //0番目のキーを取得
+    console.log(key + ":" + localStorage[key]);
+}
+
+//BGM流す
+document.getElementById("sound5").loop = true;
+if (mute != 1) {
+    document.getElementById("sound5").volume = 0.5;
+}
+$('#sound5').get(0).play();
+
+//キー操作無効
+$(window).keydown(function() {
+    return false;
+});
+
 $("#de").click(function() {
     var del = confirm("全てのデータを初期化します。よろしいですか？");
     if (del) {
@@ -13,116 +42,14 @@ $("#mute").click(function() {
     document.getElementById("sound5").volume = 0;
     localStorage.setItem("mute", 1);
 });
-mute = localStorage.getItem("mute");
-console.log(mute)
-if (mute == 1) {
+if (localStorage.getItem("mute") == 1) {
     document.getElementById("sound1").volume = 0;
     document.getElementById("sound2").volume = 0;
     document.getElementById("sound3").volume = 0;
     document.getElementById("sound4").volume = 0;
     document.getElementById("sound5").volume = 0;
 }
-//初期化
-var usagi = 0;
-var kuma = 0;
-var risu = 0;
-var aja = 0;
-var tairyou = 0;
-var n = 1;
-var n2 = 1;
-var sW2 = window.innerWidth + 60;
-var sH2 = window.innerHeight + 100;
-var creimg;
-var usaran = ["image/usa (1).png", "image/usa (2).png", "image/usa (3).png", "image/usa (4).png", "image/usa (5).png", "image/usa (6).png", "image/usa (7).png"];
-for (var i = 0; i < 10; i++) {
-    var key = localStorage.key(i); //0番目のキーを取得
-    console.log("0番目のキーは" + key + "で　値は" + localStorage[key] + "です");
-}
-//BGM流す
-document.getElementById("sound5").loop = true;
-if (mute != 1) {
-    document.getElementById("sound5").volume = 0.5;
-}
-$('#sound5').get(0).play();
-//キー操作無効
-$(window).keydown(function() {
-    return false;
-});
-//データ読み込み
-var launchTimes = localStorage.getItem("launchTimes");
-console.log(launchTimes);
-////初回プレイの場合
-if (!localStorage.getItem("launchTimes")) {
-    localStorage.setItem("launchTimes", 1);
-    localStorage.setItem("totalUsagi", 0);
-    localStorage.setItem("totalKuma", 0);
-    localStorage.setItem("totalRisu", 0);
-    localStorage.setItem("totalAja", 0);
-    var totalUsagi = Number(localStorage.getItem("totalUsagi"));
-    var totalKuma = Number(localStorage.getItem("totalKuma"));
-    var totalRisu = Number(localStorage.getItem("totalRisu"));
-    var totalAja = Number(localStorage.getItem("totalAja"));
-    var infotext = "初回プレイです";
-    $("#info").html("初回プレイです");
-    setTimeout(function() {
-        $("#info").fadeOut("slow");
-        infotext = "";
-    }, 5000);
-} else {
-    ////二回目以降の場合
-    var totalUsagi = Number(localStorage.getItem("totalUsagi"));
-    var totalKuma = Number(localStorage.getItem("totalKuma"));
-    var totalRisu = Number(localStorage.getItem("totalRisu"));
-    var totalAja = Number(localStorage.getItem("totalAja"));
-    launchTimes++;
-    localStorage.setItem("launchTimes", launchTimes);
-    var infotext = launchTimes + "回目のプレイです";
-    infotext = infotext + "<br>\n今まで累計" + totalUsagi + "匹のうさぎを増やしました"
-    if (totalKuma >= 1) {
-        infotext = infotext + "<br>\n今まで累計" + totalKuma + "匹のくまを見つけました"
-    }
-    if (totalRisu >= 1) {
-        infotext = infotext + "<br>\n今まで累計" + totalRisu + "匹のりすを見つけました"
-    }
-    if (totalAja >= 1) {
-        infotext = infotext + "<br>\n今まで累計" + totalAja + "匹のあじゃを見つけました"
-    }
-    $("#info").html(infotext);
-    setTimeout(function() {
-        $("#info").fadeOut("slow");
-        infotext = "";
-    }, 5000);
-}
-//データ保存
-$(window).on('pagehide', function() {
-    console.log(launchTimes);
-    console.log(totalUsagi);
-    console.log(totalRisu);
-    console.log(totalKuma);
-    console.log(localStorage.getItem("launchTimes"));
-    console.log(localStorage.getItem("totalUsagi"));
-    console.log(localStorage.getItem("totalKuma"));
-    console.log(localStorage.getItem("totalRisu"));
-    if (localStorage.getItem("launchTimes") == 1) {
-        var totalUsagi = usagi;
-        var totalKuma = kuma;
-        var totalRisu = risu;
-        var totalAja = aja;
-    } else {
-        var totalUsagi = Number(localStorage.getItem("totalUsagi"));
-        var totalKuma = Number(localStorage.getItem("totalKuma"));
-        var totalRisu = Number(localStorage.getItem("totalRisu"));
-        var totalAja = Number(localStorage.getItem("totalAja"));
-        totalUsagi = totalUsagi + usagi;
-        totalKuma = totalKuma + kuma;
-        totalRisu = totalRisu + risu;
-        totalAja = totalAja + aja;
-    }
-    localStorage.setItem("totalUsagi", totalUsagi);
-    localStorage.setItem("totalKuma", totalKuma);
-    localStorage.setItem("totalRisu", totalRisu);
-    localStorage.setItem("totalAja", totalAja);
-})
+
 //ボタンクリック
 $("#1").click(function() {
     usafuya();
---
+++ docs/usapyon_data.js
@@ -0,0 +1,75 @@
+//データ読み込み
+var launchTimes = localStorage.getItem("launchTimes");
+console.log(launchTimes);
+////初回プレイの場合
+if (!localStorage.getItem("launchTimes")) {
+    localStorage.setItem("launchTimes", 1);
+    localStorage.setItem("totalUsagi", 0);
+    localStorage.setItem("totalKuma", 0);
+    localStorage.setItem("totalRisu", 0);
+    localStorage.setItem("totalAja", 0);
+    var totalUsagi = Number(localStorage.getItem("totalUsagi"));
+    var totalKuma = Number(localStorage.getItem("totalKuma"));
+    var totalRisu = Number(localStorage.getItem("totalRisu"));
+    var totalAja = Number(localStorage.getItem("totalAja"));
+    var infotext = "初回プレイです";
+    $("#info").html("初回プレイです");
+    setTimeout(function() {
+        $("#info").fadeOut("slow");
+        infotext = "";
+    }, 5000);
+} else {
+    ////二回目以降の場合
+    var totalUsagi = Number(localStorage.getItem("totalUsagi"));
+    var totalKuma = Number(localStorage.getItem("totalKuma"));
+    var totalRisu = Number(localStorage.getItem("totalRisu"));
+    var totalAja = Number(localStorage.getItem("totalAja"));
+    launchTimes++;
+    localStorage.setItem("launchTimes", launchTimes);
+    var infotext = launchTimes + "回目のプレイです";
+    infotext = infotext + "<br>\n今まで累計" + totalUsagi + "匹のうさぎを増やしました"
+    if (totalKuma >= 1) {
+        infotext = infotext + "<br>\n今まで累計" + totalKuma + "匹のくまを見つけました"
+    }
+    if (totalRisu >= 1) {
+        infotext = infotext + "<br>\n今まで累計" + totalRisu + "匹のりすを見つけました"
+    }
+    if (totalAja >= 1) {
+        infotext = infotext + "<br>\n今まで累計" + totalAja + "匹のあじゃを見つけました"
+    }
+    $("#info").html(infotext);
+    setTimeout(function() {
+        $("#info").fadeOut("slow");
+        infotext = "";
+    }, 5000);
+}
+//データ保存
+$(window).on('pagehide', function() {
+    console.log(launchTimes);
+    console.log(totalUsagi);
+    console.log(totalRisu);
+    console.log(totalKuma);
+    console.log(localStorage.getItem("launchTimes"));
+    console.log(localStorage.getItem("totalUsagi"));
+    console.log(localStorage.getItem("totalKuma"));
+    console.log(localStorage.getItem("totalRisu"));
+    if (localStorage.getItem("launchTimes") == 1) {
+        var totalUsagi = usagi;
+        var totalKuma = kuma;
+        var totalRisu = risu;
+        var totalAja = aja;
+    } else {
+        var totalUsagi = Number(localStorage.getItem("totalUsagi"));
+        var totalKuma = Number(localStorage.getItem("totalKuma"));
+        var totalRisu = Number(localStorage.getItem("totalRisu"));
+        var totalAja = Number(localStorage.getItem("totalAja"));
+        totalUsagi = totalUsagi + usagi;
+        totalKuma = totalKuma + kuma;
+        totalRisu = totalRisu + risu;
+        totalAja = totalAja + aja;
+    }
+    localStorage.setItem("totalUsagi", totalUsagi);
+    localStorage.setItem("totalKuma", totalKuma);
+    localStorage.setItem("totalRisu", totalRisu);
+    localStorage.setItem("totalAja", totalAja);
+})
