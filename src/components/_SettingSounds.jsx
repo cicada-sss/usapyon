361090fd8410e3473ebaa4d800e4334c28f771f3
cicada_sss <sss.cicada@gmail.com>
2019-06-02 15:05:25 +0900

    Fix SettingSounds

--- src/components/_SettingSounds.jsx
+++ src/components/_SettingSounds.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import MuteSE from "./components/_MuteSE.jsx";
-import MuteBGM from "./components/_MuteBGM.jsx";
-import PlayKorobushka from "./components/_PlayKorobushka.jsx";
-import PlayCarmen from "./components/_PlayCarmen.jsx";
-import PlayUsapri from "./components/_PlayUsapri.jsx";
+import MuteSE from "./_MuteSE.jsx";
+import MuteBGM from "./_MuteBGM.jsx";
+import PlayKorobushka from "./_PlayKorobushka.jsx";
+import PlayCarmen from "./_PlayCarmen.jsx";
+import PlayUsapri from "./_PlayUsapri.jsx";
 
 const SettingSounds = () => {
   return (
     <section>
       <h1>音設定</h1>
       <MuteSE />
+      <br />
       <MuteBGM />
+      <br />
       <PlayKorobushka />
+      <br />
       <PlayCarmen />
+      <br />
       <PlayUsapri />
     </section>
   );
-}
+};
 
 ReactDOM.render(<SettingSounds />, document.getElementById("setting-sounds"));
