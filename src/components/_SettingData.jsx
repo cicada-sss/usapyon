f0d2e60a9d5776d16aaec0c1778a3e68a44b5f49
cicada_sss <sss.cicada@gmail.com>
2019-06-02 15:04:36 +0900

    Add SettingData

---
+++ src/components/_SettingData.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import DeleteButton from "./components/_DeleteButton.jsx";
+
+const SettingData = () => {
+  return (
+    <section>
+      <h1>設定</h1>
+      <DeleteButton />
+    </section>
+  );
+}
+
+export default SettingData;
