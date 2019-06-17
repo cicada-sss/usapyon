45ec9e64691b2f514488bd291a0381e93b7ef9cc
cicada_sss <sss.cicada@gmail.com>
2019-06-17 12:25:15 +0900

    Fix loadSounds to export vars

--- src/loadSounds.js
+++ src/loadSounds.js
@@ -1,33 +1,42 @@
-let bgm1 = new Audio("sound/carmen.mp3");
-bgm1.preload = "auto";
-bgm1.loop = true;
-let bgm2 = new Audio("sound/jupiter.mp3");
-bgm2.preload = "auto";
-let bgm3 = new Audio("sound/Коробейники.mp3");
-bgm3.preload = "auto";
-bgm3.volume = 0.5;
-bgm3.loop = true;
-let bgm4 = new Audio("sound/usapri.mp3");
-bgm4.preload = "auto";
-bgm4.volume = 0.5;
-bgm4.loop = true;
-let se1 = new Audio("sound/usa.mp3");
-se1.preload = "auto";
-let se2 = new Audio("sound/alert.mp3");
-se2.preload = "auto";
-let se3 = new Audio("sound/trumpet.mp3");
-se3.preload = "auto";
-let se4 = new Audio("sound/dame.mp3");
-se4.preload = "auto";
+const bgm = [
+  new Audio("sound/carmen.mp3"),
+  new Audio("sound/jupiter.mp3"),
+  new Audio("sound/Коробейники.mp3"),
+  new Audio("sound/usapri.mp3")
+];
+
+const se = [
+  new Audio("sound/usa.mp3"),
+  new Audio("sound/alert.mp3"),
+  new Audio("sound/trumpet.mp3"),
+  new Audio("sound/dame.mp3")
+];
+
+bgm[0].preload = "auto";
+bgm[0].loop = true;
+bgm[1].preload = "auto";
+bgm[2].preload = "auto";
+bgm[2].volume = 0.5;
+bgm[2].loop = true;
+bgm[3].preload = "auto";
+bgm[3].volume = 0.5;
+bgm[3].loop = true;
+se[0].preload = "auto";
+se[1].preload = "auto";
+se[2].preload = "auto";
+se[3].preload = "auto";
+
 if (localStorage.usapri == 1) {
-  bgm4.play();
+  bgm[3].play();
 } else {
-  bgm3.play();
+  bgm[2].play();
 }
 
-let stopBGM = () => {
-  bgm1.pause();
-  bgm2.pause();
-  bgm3.pause();
-  bgm4.pause();
-}
+const stopBGM = () => {
+  bgm[0].pause();
+  bgm[1].pause();
+  bgm[2].pause();
+  bgm[3].pause();
+};
+
+export { bgm, se, stopBGM };
